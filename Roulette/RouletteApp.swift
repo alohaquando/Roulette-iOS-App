1b5b4bb043b4b64de9e2fda9f7bd9c93b1b2dd5a
aloha.quan <aloha.quando@gmail.com>
2022-08-16 15:59:23 +0700

    Initial Commit

---
+++ Roulette/RouletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouletteApp.swift
+//  Roulette
+//
+//  Created by Quân Đỗ on 8/16/22.
+//
+
+import SwiftUI
+
+@main
+struct RouletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
